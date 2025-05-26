87666f6abdb35fc5c766fb392c9064f3213b645e
Shubbu03 <thatcoderguyshubham@gmail.com>
2025-05-26 10:40:14 +0530

    redirect if authenticated and dashboard init

--- apps/web/app/(auth)/login/page.tsx
+++ apps/web/app/(auth)/login/page.tsx
@@ -1,7 +1,14 @@
 "use client";
 
 import AuthForm from "../../../components/AuthForm";
+import { useSession } from "next-auth/react";
+import { redirect } from "next/navigation";
 
 export default function Login() {
+  const session = useSession();
+
+  if (session.status === "authenticated") {
+    redirect("/dashboard");
+  }
   return <AuthForm mode="login" />;
 }
