be67a2f52f26745349ef17ceb22661b727130086
Shubbu03 <thatcoderguyshubham@gmail.com>
2025-05-28 22:45:02 +0530

    api for fetching all room and checking valid exisitng room

---
+++ apps/web/app/api/fetch-existing-canvas/route.ts
@@ -0,0 +1,36 @@
+import { getServerSession } from "next-auth";
+import { NextResponse } from "next/server";
+import { authOptions } from "../auth/[...nextauth]/options";
+import { prisma } from "@repo/db/prisma";
+
+export async function GET() {
+  try {
+    const session = await getServerSession(authOptions);
+
+    if (!session || !session.user) {
+      return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
+    }
+
+    const userID = session.user.id;
+
+    const existingCanvas = await prisma.room.findMany({
+      where: { adminID: userID },
+      select: {
+        id: true,
+        slug: true,
+        createdAt: true,
+      },
+    });
+
+    return NextResponse.json({
+      message: "Canvas fetched successfully",
+      status: 200,
+      canvas: existingCanvas,
+    });
+  } catch (error) {
+    return NextResponse.json({
+      error: "Error occured while fetching canvas data",
+      status: 500,
+    });
+  }
+}
