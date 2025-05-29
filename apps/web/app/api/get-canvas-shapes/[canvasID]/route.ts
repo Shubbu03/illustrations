f2db60043a5c2c0b81b3de8dc67dd7c6860dfc76
Shubbu03 <thatcoderguyshubham@gmail.com>
2025-05-29 16:33:36 +0530

    refactor: replace get-canvas-shapes API with canvasID-specific endpoint and add slug-based canvas ID retrieval

---
+++ apps/web/app/api/get-canvas-shapes/[canvasID]/route.ts
@@ -0,0 +1,54 @@
+import { prisma } from "@repo/db/prisma";
+import { getServerSession } from "next-auth";
+import { NextRequest, NextResponse } from "next/server";
+import { authOptions } from "../../auth/[...nextauth]/options";
+
+export async function GET(
+  request: NextRequest,
+  { params }: { params: Promise<{ canvasID: string }> }
+) {
+  try {
+    const canvasID = (await params).canvasID;
+
+    if (!canvasID) {
+      return NextResponse.json(
+        { error: "Canvas ID is required" },
+        { status: 400 }
+      );
+    }
+
+    const session = await getServerSession(authOptions);
+
+    if (!session || !session.user) {
+      return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
+    }
+
+    const shapes = await prisma.chat.findMany({
+      where: {
+        roomID: Number(canvasID),
+      },
+      orderBy: {
+        id: "desc",
+      },
+      take: 50,
+      select: {
+        message: true,
+      },
+    });
+
+    if (!shapes) {
+      return NextResponse.json({ error: "Room not found" }, { status: 404 });
+    }
+
+    return NextResponse.json({
+      message: "Canvas shapes fetched",
+      shapes: shapes,
+      status: 200,
+    });
+  } catch (error) {
+    return NextResponse.json(
+      { error: "Failed to fetch chats" },
+      { status: 500 }
+    );
+  }
+}
---
+++ apps/web/app/api/get-canvasID/[slug]/route.ts
@@ -0,0 +1,45 @@
+import { prisma } from "@repo/db/prisma";
+import { getServerSession } from "next-auth";
+import { NextRequest, NextResponse } from "next/server";
+import { authOptions } from "../../auth/[...nextauth]/options";
+
+export async function GET(
+  request: NextRequest,
+  { params }: { params: Promise<{ slug: string }> }
+) {
+  try {
+    const slug = (await params).slug;
+
+    if (!slug) {
+      return NextResponse.json({ error: "Slug is required" }, { status: 400 });
+    }
+
+    const session = await getServerSession(authOptions);
+
+    if (!session || !session.user) {
+      return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
+    }
+
+    const canvasID = await prisma.room.findUnique({
+      where: { slug },
+      select: {
+        id: true,
+      },
+    });
+
+    if (!canvasID) {
+      return NextResponse.json({ error: "Canvas not found" }, { status: 404 });
+    }
+
+    return NextResponse.json({
+      message: "Canvas id fetched",
+      id: canvasID.id,
+      status: 200,
+    });
+  } catch (error) {
+    return NextResponse.json(
+      { error: "Failed to fetch chats" },
+      { status: 500 }
+    );
+  }
+}
